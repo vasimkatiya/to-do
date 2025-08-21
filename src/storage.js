e33ba31f40ada8cb4580920c5fe153b818aa361c
vasimkatiya <vasimkatiya97@gmail.com>
2025-08-21 17:33:01 +0530

    Update storage.js

--- src/storage.js
+++ src/storage.js
@@ -1,22 +1,4 @@
 
-// // storage.js
-// import { projectArray, GlobalTasks } from "./project";
-
-// export function saveLocal() {
-//   localStorage.setItem("projects", JSON.stringify(projectArray));
-//   localStorage.setItem("tasks", JSON.stringify(GlobalTasks));
-// }
-
-// export function loadLocal() {
-//   const projects = JSON.parse(localStorage.getItem("projects")) || [];
-//   const tasks = JSON.parse(localStorage.getItem("tasks")) || [];
-
-//   projectArray.length = 0;
-//   GlobalTasks.length = 0;
-
-//   projects.forEach(p => projectArray.push(p));
-//   tasks.forEach(t => GlobalTasks.push(t));
-// }
 
 
 import { Project, projectArray, GlobalTasks } from "./project";
@@ -30,16 +12,16 @@ export function loadLocal() {
   const projects = JSON.parse(localStorage.getItem("projects")) || [];
   const tasks = JSON.parse(localStorage.getItem("tasks")) || [];
 
-  // clear old arrays
+  
   projectArray.length = 0;
   GlobalTasks.length = 0;
 
-  // rebuild proper Project + Task objects
+  
   projects.forEach(p => {
     const project = new Project(p.name);
     project.id = p.id;
     p.tasks.forEach(t => {
-      project.addTask(t); // this also pushes into GlobalTasks
+      project.addTask(t); 
     });
     projectArray.push(project);
   });
